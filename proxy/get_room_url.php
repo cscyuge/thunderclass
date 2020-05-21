ead42a60dccc18d6cd3a3cdcb332235fb3f0aa7a
linjunxian <cscyuge@github.com>
2020-05-21 17:32:38 +0800

    go to class

--- proxy/get_room_url.php
+++ proxy/get_room_url.php
@@ -2,10 +2,10 @@
 include 'http_request.php';
 ini_set('display_errors','On');
 error_reporting(E_ALL);
-$method = 'GET';
+$method = 'POST';
 $headers = array("Content-type" => "application/json", "charset" => "utf-8");
 
-$url = "http://47.94.201.157:9090/api/dkt/get_room_url";
+$url = "http://47.94.201.157:9090/api/dkt/live/get_room_url/";
 $res = http_request($method,$url,$_POST,$headers);
 $res = json_encode($res);
 echo $res;
