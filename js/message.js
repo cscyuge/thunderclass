c66bb8c8bd8785f56508f1c9643d749c44ca3007
dutWangjiaqi <1512926647@qq.com>
2020-06-02 16:30:49 +0800

    mess2

--- js/message.js
+++ js/message.js
@@ -71,7 +71,7 @@ function get_user_msg(){
     let token = localStorage.getItem("token");
     let account = localStorage.getItem("account");
     let url =  'proxy/get_msg.php';
-    let data = {account: account,communicator:"guiyutong.sky", token: token};
+    let data = {account: account,communicator:"all", token: token};
     let res = http_request(url,data);
     let msg_list = null;
     if (res){
@@ -80,7 +80,6 @@ function get_user_msg(){
             msg_list = data;
             console.log("1");
             console.log(data);
-            console.log("2");
             localStorage.setItem("msg_list1",JSON.stringify(data));
         }else if (res.code === 500){
             restart();
@@ -105,21 +104,29 @@ function show_msgs(){
     console.log("0");
 }
 function show_user_msgs(){
-    console.log("3");
     let msg_list1 = get_user_msg();
     console.log("4");
+    let ulObj1 = document.createElement("ul");
     let ulObj2 = document.createElement("ul");
     for (let i = 0, n = msg_list1.length; i < n; i++) {
         let msg = msg_list1[i];
+        let liObj1 = document.createElement("li");
         let liObj2 = document.createElement("li");
+        let tempHTML1;
         let tempHTML2;
-        tempHTML2 = create_msg_div3(msg);
+        if(msg.sender=="jiangshuiping.sky")
+        { tempHTML1 = create_msg_div3(msg);}
+        if(msg.sender=="guiyutong.sky")
+        {tempHTML2 = create_msg_div3(msg);}
         console.log("6");
         localStorage.setItem("person_name", msg.sender);
+        liObj1.innerHTML = tempHTML1;
         liObj2.innerHTML = tempHTML2;
+        ulObj1.appendChild(liObj1);
         ulObj2.appendChild(liObj2);
     }
-    $("#course_list2").append(ulObj2);
+    $("#course_list2").append(ulObj1);
+    $("#hidden_enent1").append(ulObj2);
 }
 function create_msg_div(msg) {
     let html = "";
