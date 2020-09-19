727312b720e84f9372f5993e14f6dec3d5f6f5de
dutWangjiaqi <1512926647@qq.com>
2020-09-19 19:12:30 +0800

    mess

--- js/message.js
+++ js/message.js
@@ -238,12 +238,14 @@ function friend_list(value) {
     console.log("77.5");
 //<img src="images/portrait.png" class="portrait" onclick="" alt="info">
    // let temp1 = "<img src = p class= c onclick = f >"
-    let temp1 = " <img src=\"images/portrait.png\" class=\"portrait\" onclick= \"show_friend_msg(value)\" alt=\"info\"> ";
+    let temp1 = " <button onclick='show_friend_msg(value)'> ";
    // temp1 = temp1.replace(/p/,  "images/portrait.png");
    // temp1 = temp1.replace(/c/,  "portrait");
    // temp1 = temp1.replace(/f/, "show_friend_msg(value)");
     console.log("88");
     html += temp1;
+    temp1="按我</button>";
+    html+=temp1;
     // var button = "<button onclick=\"check(\'course_id\')\">"+"check"+"</button>";
     //button=button.replace(/course_id/,course.course_id);
     // html += button;*/
